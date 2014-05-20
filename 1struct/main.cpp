939eee0c8e80e570463923ae309626358a4fc3ff
SKB-Hnure <diana112358@mail.ru>
2014-05-21 00:52:55 +0300

    test

---
+++ 1struct/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    struct phone
+    {
+    int town;
+    int number_station;
+    int number_phone;
+    };
+    phone my_number={212,767,8900};
+    phone number;
+    cout<<"Enter code town, numer station and number subscriber";
+    cin>>number.town>>number.number_station>>number.number_phone;
+    cout<<my_number.town<<'\t'<<my_number.number_station<<'\t'<<my_number.number_phone<<endl;
+    cout<<number.town<<'\t'<<number.number_station<<'\t'<<number.number_phone;
+    return 0;
+}
---
+++ 2struct/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+struct fraction {
+          double numerator;
+          double denominator;
+};
+
+fraction fadd(fraction a, fraction b)
+{
+ fraction result;
+ result.denominator=(a.numerator*b.denominator+b.numerator*a.denominator);
+ result.numerator=(a.denominator*b.denominator);
+ return result;
+
+}
+
+int main()
+{
+ fraction operand[2];
+ double result;
+ char operation;
+ cout<<"Введите выражение: ";
+ cin>>operand[0].numerator>>operand[0].denominator;
+cin>>operation;
+cin>>operand[1].numerator>>operand[1].denominator;
+  switch (operation)
+ {
+ case '+':result=(operand[0].numerator*operand[1].denominator+operand[1].numerator*operand[0].denominator)/(operand[0].denominator*operand[1].denominator);
+ break;
+ case '-':result=(operand[0].numerator*operand[1].denominator-operand[0].denominator*operand[1].numerator)/(operand[0].denominator*operand[1].denominator);
+ break;
+ case '*':result=(operand[0].numerator/operand[0].denominator)*(operand[1].numerator/operand[1].denominator);
+ break;
+ case '/':result=(operand[0].numerator/operand[0].denominator)/(operand[1].numerator/operand[1].denominator);
+ break;
+ default:
+ cout<<"mistake";
+ break;
+ }
+ cout<<"\n";
+ cout<<operand[0].numerator<<"/"<<operand[0].denominator<<operation<<operand[1].numerator<<"/"<<operand[1].denominator<<"="<<result;
+
+    return 0;
+}
